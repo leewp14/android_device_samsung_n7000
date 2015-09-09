9b06e2be25ff53a4cff2ea4114c25e3e7bac596c
bauner <bauner@gmx.de>
2015-09-09 12:41:07 +0200

    use arm-eabi-4.4.3 for kernel building

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,10 @@ TARGET_BOARD_INFO_FILE := device/samsung/n7000/board-info.txt
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/n7000/bluetooth
 
+#Use old kernel toolchain. Newer GCC may be the root cause of our modem
+#interface dying sometimes.
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+
 # Inline kernel building
 # TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_SOURCE := kernel/samsung/tw_n7000
