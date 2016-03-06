44fa140a99260ea60a69e0b2901ddfd253abe9d2
Dominggoes Isakh <drjisakh@gmail.com>
2016-03-06 21:02:50 +0100

    i9100: Implement significant motion-sensor by using accelerometer.
    We use the accelerometer to detect motion, but this does not
    follow the required specs (especially power) for implementation.
    But the upside is that Doze can enter deep-idle.
    
    Change-Id: I4c940efebe6eecb53544871494ab5663fa213767

--- libsensors/sensors.cpp
+++ libsensors/sensors.cpp
@@ -61,6 +61,7 @@
 #define SENSORS_PROXIMITY_HANDLE        4
 #define SENSORS_GYROSCOPE_HANDLE        5
 #define SENSORS_PRESSURE_HANDLE         6
+#define SENSORS_SIGNIFICANT_MOTION_HANDLE 7
 
 #define AKM_FTRACE 0
 #define AKM_DEBUG 0
@@ -105,6 +106,11 @@ static const struct sensor_t sSensorList[] = {
           1, SENSORS_PRESSURE_HANDLE,
           SENSOR_TYPE_PRESSURE, 1100.0f, 0.01f, 0.67f, 20000, 0, 0,
           "", "", 0, 0, {0, 0}},
+        { "Significant motion sensor (Accelerometer)",
+          "STMicroelectronics",
+          1, SENSORS_SIGNIFICANT_MOTION_HANDLE,
+          SENSOR_TYPE_SIGNIFICANT_MOTION, 1.0f, 1.0f, 0.23f, 0, 0, 0,
+          "", "", 0, SENSOR_FLAG_WAKE_UP, {0, 0},},
 };
 
 
@@ -168,6 +174,7 @@ private:
             case ID_A:
             case ID_M:
             case ID_O:
+            case ID_SM:
                 return akm;
             case ID_P:
                 return proximity;
