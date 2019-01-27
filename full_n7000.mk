f31c1484e3657c54dc544d62608dbd5adcac2b45
leewp14 <lee.wp14@gmail.com>
2019-01-27 19:01:28 +0800

    We are on RROS!
    Change-Id: Ibfb56e17f8f22a0b01b2ba3992d4477e72a2ba67

--- full_n7000.mk
+++ full_n7000.mk
@@ -23,7 +23,9 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, device/samsung/n7000/n7000.mk)
+
+# Inherit from n7000 device
+$(call inherit-product, device/samsung/n7000/device.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_n7000
--- rr.mk
+++ rr.mk
@@ -17,15 +17,15 @@
 # Release name
 PRODUCT_RELEASE_NAME := GT-N7000
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common RR stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/n7000/full_n7000.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n7000
-PRODUCT_NAME := lineage_n7000
+PRODUCT_NAME := rr_n7000
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-N7000
 
